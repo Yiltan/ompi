daa69a8eafc67450c364e498e304df9bca4b01cd
Yiltan Hassan Temucin <yiltan.github@gmail.com>
2020-06-16 09:39:36 -0400

    Added CUDA kernel reduction and modified MLNX install settings

--- ompi/op/op.h
+++ ompi/op/op.h
@@ -44,10 +44,10 @@
 #include "ompi/mpi/fortran/base/fint_2_int.h"
 #include "ompi/mca/op/op.h"
 
-#if OPAL_CUDA_SUPPORT
+#include "kernel.h"
+
 #include "opal/datatype/opal_convertor.h"
 #include "opal/datatype/opal_datatype_cuda.h"
-#endif
 
 BEGIN_C_DECLS
 
@@ -550,12 +550,11 @@ static inline void ompi_op_reduce(ompi_op_t * op, void *source,
                                   ompi_datatype_t * dtype)
 {
 
-#if OPAL_CUDA_SUPPORT
     if (opal_cuda_check_bufs((char *) source, (char *) target))
     {
+      vecAdd((float *) source, (float *) target, count);
       return;
     }
-#endif
 
     MPI_Fint f_dtype, f_count;
 
