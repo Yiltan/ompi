f1640dcf4a833423236c480ef150c6c993e1b7fc
Yiltan Hassan Temucin <yiltan.github@gmail.com>
2020-07-06 11:26:31 -0400

    Dev: added envioment variable to enable/disable UCX modification. Returned code to original state if disabled

--- ompi/mca/coll/cuda/coll_cuda_allreduce.c
+++ ompi/mca/coll/cuda/coll_cuda_allreduce.c
@@ -16,13 +16,29 @@
 #include <stdio.h>
 
 #include "ompi/op/op.h"
+#include "opal/datatype/opal_convertor.h"
+#include "opal/datatype/opal_datatype_cuda.h"
+
+int ucx_aware_status = -1;
+static inline int is_ucx_aware() {
+  if (ucx_aware_status == -1) {
+    char* env = getenv("IS_UCX_AWARE");
+
+    if (NULL == env) {
+      *env = '0';
+    }
+
+    ucx_aware_status = atoi(env);
+  }
+  return ucx_aware_status;
+}
 
 /*
- *	allreduce_intra
+ * allreduce_intra
  *
- *	Function:	- allreduce using other MPI collectives
- *	Accepts:	- same as MPI_Allreduce()
- *	Returns:	- MPI_SUCCESS or error code
+ * Function: - allreduce using other MPI collectives
+ *  Accepts:  - same as MPI_Allreduce()
+ * Returns:  - MPI_SUCCESS or error code
  */
 int
 mca_coll_cuda_allreduce(const void *sbuf, void *rbuf, int count,
@@ -31,8 +47,50 @@ mca_coll_cuda_allreduce(const void *sbuf, void *rbuf, int count,
                         struct ompi_communicator_t *comm,
                         mca_coll_base_module_t *module)
 {
-    mca_coll_cuda_module_t *s = (mca_coll_cuda_module_t*) module;
-    return s->c_coll.coll_allreduce(sbuf, rbuf, count, dtype, op, comm,
-                                         s->c_coll.coll_allreduce_module);
-}
+  mca_coll_cuda_module_t *s = (mca_coll_cuda_module_t*) module;
 
+  if (is_ucx_aware() && OMPI_OP_SUM == op->op_type) {
+      return s->c_coll.coll_allreduce(sbuf, rbuf, count,
+                                      dtype, op, comm,
+                                      s->c_coll.coll_allreduce_module);
+  }
+
+  ptrdiff_t gap;
+  char *rbuf1 = NULL, *sbuf1 = NULL, *rbuf2 = NULL;
+  size_t bufsize;
+  int rc;
+
+  bufsize = opal_datatype_span(&dtype->super, count, &gap);
+
+  if ((MPI_IN_PLACE != sbuf) && (opal_cuda_check_bufs((char *)sbuf, NULL))) {
+      sbuf1 = (char*)malloc(bufsize);
+      if (NULL == sbuf1) {
+          return OMPI_ERR_OUT_OF_RESOURCE;
+      }
+      opal_cuda_memcpy_sync(sbuf1, sbuf, bufsize);
+      sbuf = sbuf1 - gap;
+  }
+
+  if (opal_cuda_check_bufs(rbuf, NULL)) {
+      rbuf1 = (char*)malloc(bufsize);
+      if (NULL == rbuf1) {
+          if (NULL != sbuf1) free(sbuf1);
+          return OMPI_ERR_OUT_OF_RESOURCE;
+      }
+      opal_cuda_memcpy_sync(rbuf1, rbuf, bufsize);
+      rbuf2 = rbuf; /* save away original buffer */
+      rbuf = rbuf1 - gap;
+  }
+
+  rc = s->c_coll.coll_allreduce(sbuf, rbuf, count, dtype, op, comm, s->c_coll.coll_allreduce_module);
+
+  if (NULL != sbuf1) {
+      free(sbuf1);
+  }
+  if (NULL != rbuf1) {
+      rbuf = rbuf2;
+      opal_cuda_memcpy_sync(rbuf, rbuf1, bufsize);
+      free(rbuf1);
+  }
+  return rc;
+}
